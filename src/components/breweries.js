12f1370d9d88f2a99b85accf8a9dc89aa1a98e89
EvanRPavone <evanrpavone@gmail.com>
2021-01-30 22:56:30 -0500

    Delete Button - Requirements met

--- src/components/breweries.js
+++ src/components/breweries.js
@@ -1,108 +1,165 @@
 class Breweries {
     constructor() {
-      this.breweries = [];
-      this.adapter = new BreweriesAdapter();
-      this.formSubmit = document.getElementById("form-submit");
-      this.formButtons = document.getElementById("form-show-buttons");
-      this.addBreweryButton = document.getElementById("add-brewery");
-      this.removeBreweryButton = document.getElementById("remove-brewery");
-      // this.editBreweryButton = document.getElementById("edit-brewery");
-      this.cardContainer = document.getElementById('brewery-card-container');
-      this.bindEventListeners();
-      this.fetchAndLoadBreweries();
-    }
-  
-    fetchAndLoadBreweries() {
-      this.adapter.getBreweries().then(breweries => this.createBreweries(breweries)).then(() => this.addBreweriesToDom())
-    }
-  
-    bindEventListeners() {
-      const editBreweryButton = document.getElementById("edit-brewery");
-      this.formSubmit.addEventListener("click", function() {
-        event.preventDefault();
-        this.addBrewery();
-      }.bind(this))
-      this.addBreweryButton.addEventListener("click", function() {
-        this.toggleForm();
-        this.toggleButtons();
-      }.bind(this))
-      if (editBreweryButton) {
-        editBreweryButton.addEventListener("click",function() {
-          console.log("Edit")
-        }.bind(this))
-      }
-      // if (this.removeBreweryButton) {
-      //   this.removeBreweryButton.addEventListener("click", function() {
-      //     this.adapter.removeBrewery();
-      //   }.bind(this))
-      // }
-    }
-  
-    createArrayOfBreweryFavorites(favorites) {
-      let favoriteArray = [];
-      for (let favorite of favorites) {
-        favoriteArray.push(favorite.name);
-      }
-      console.log("favorite beer added to favoriteArray" + favoriteArray)
-      return favoriteArray
-    }
-  
-    createBreweries(breweries) {
-      for (let brewery of breweries) {
-        let favorites = this.createArrayOfBreweryFavorites(brewery.attributes.favorites)
-        this.breweries.push(new Brewery(brewery.attributes.id, brewery.attributes.name, brewery.attributes.city, brewery.attributes.state, favorites))
-        console.log("createBreweries Function - adds brewery name, city, state and favorites")
-      }
-    }
-  
-    addBreweriesToDom() {
-      for (let brewery of this.breweries) {
-        brewery.createBreweryCard()
-      }
-    }
-  
-    addBrewery() {
-      const form = event.target.parentElement
-      const favorites = form[3].value.split(', ')
-      const brewery = new Brewery(form[0].value, form[1].value, form[2].value, favorites)
-      const configurationObject = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify({
-          "name": form[0].value,
-          "city": form[1].value,
-          "state": form[2].value,
-          "favorites": favorites
-        })
-      };
-      this.adapter.postBreweryToApi(configurationObject).then(function(json) {
-        brewery.createBreweryCard();
-        this.toggleButtons();
-        this.toggleForm();
-      }.bind(this))
+        this.breweries = [];
+        this.favorites = [];
+        this.baseURL = "http://localhost:3000/breweries";
+        this.favoritesURL = "http://localhost:3000/favorites"
+        this.formSubmit = document.querySelector("#form-submit");
+        this.formButtons = document.querySelector("#form-show-buttons");
+        this.addBreweryButton = document.querySelector("#add-brewery");
+        this.removeBreweryButton = document.querySelector("#remove-breweries");
+        this.cardContainer = document.querySelector("#brewery-card-container");
+        this.fetchAndLoadBreweries();
+        this.bindEventListeners();
     }
 
+    fetchAndLoadBreweries(){
+        return fetch(this.baseURL)
+        .then(response => response.json())
+        .then(json => json.data)
+        .then(breweries => this.createBreweries(breweries))
+        .then(() => this.addBreweriesToDom());
+    }
+
+    bindEventListeners() {
+        this.formSubmit.addEventListener("click", function() {
+            event.preventDefault();
+            this.addBrewery();
+            this.createBreweries(this.breweries);
+        }.bind(this));
+        this.addBreweryButton.addEventListener("click", function() {
+            this.toggleForm();
+            this.toggleButtons();
+        }.bind(this));
+        this.removeBreweryButton.addEventListener("click", function() {
+            alert("This will Delete all Breweries");
+            this.removeBreweries();
+        }.bind(this));
+    }
+
+    // CREATING BREWERIES - POST
+
+    createArrayOfBreweryFavorites(favorites) {
+        let favoriteArray = [];
+        for (let favorite of favorites) {
+          favoriteArray.push(favorite.beer);
+        }
+        return favoriteArray;
+    }
+
+    createBreweries(breweries) {
+    
+        for (let brewery of breweries) {
+            let beers = this.createArrayOfBreweryFavorites(brewery.attributes.favorites);
+            this.breweries.push(new Brewery(brewery.attributes.name, brewery.attributes.city, brewery.attributes.state, beers));
+        }
+    }
+
+    addBrewery() {
+        const form = event.target.parentElement;
+        const favorites = form[3].value.split(', ');
+        const brewery = new Brewery(form[0].value, form[1].value, form[2].value, favorites);
+        const configurationObject = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                "name": form[0].value,
+                "city": form[1].value,
+                "state": form[2].value,
+                "favorites": favorites
+            })
+        };
+        fetch(this.baseURL, configurationObject)
+        .then(resp => resp.json())
+        .catch(error => console.log("Error: " + error))
+        .then(function(json) {
+            brewery.createBreweryCard();
+            this.toggleButtons();
+            this.toggleForm();
+        }.bind(this));
+    }
+
+    addBreweriesToDom() {
+        for (let brewery of this.breweries) {
+            brewery.createBreweryCard();
+        };
+    }
+
+    // DELETE BREWERIES
+
+    removeBreweries() {
+        fetch(this.baseURL, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+        })
+        this.removeBreweryCards();
+        this.deleteBreweryFromApi();
+        this.removeFavorites();
+    }
+
+    removeBreweryCards() {
+        const allCards = document.querySelector("#brewery-card-container");
+        allCards.innerHTML = "";
+    }
+
+    deleteBreweryFromApi() {
+        return fetch(this.baseURL)
+        .then(resp => resp.json())
+        .catch(error => console.log("Error: " + error));
+    }
+
+    // FAVORITES
+
+    getFavorites(){
+        return fetch(this.favoritesURL)
+        .then(resp => resp.json())
+        .then(json => (json.data));
+    }
+
+    removeFavorites() {
+        console.log("Hello")
+        fetch(this.favoritesURL, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+        })
+        console.log("removing favorites");
+        this.removeFavoritesFromApi();
+        console.log("Removing All Favorites Now");
+    }
+
+    removeFavoritesFromApi() {
+        console.log("Trying To remove Favorites from API");
+        return fetch(this.favoritesURL)
+        .then(resp => resp.json())
+        .catch(error => console.log("Error: " + error));
+        console.log("Successfully Removed from API")
+    }
+
+    // Form Control
+
     hideOrShowElement(element) {
-      if (element.classList.contains("hidden")) {
-        element.classList.remove("hidden");
-      } else {
-        element.className += " hidden";
-      }
+        if (element.classList.contains("hidden")) {
+            element.classList.remove("hidden");
+        } else {
+            element.className += " hidden";
+        }
     }
-  
+
     toggleForm() {
-      const form = this.formSubmit.parentElement;
-      this.hideOrShowElement(form);
+        const form = this.formSubmit.parentElement;
+        this.hideOrShowElement(form);
     }
-  
+
     toggleButtons() {
-      this.hideOrShowElement(this.formButtons);
-    }
-  
-    clearBreweries() {
-      this.cardContainer.innerHTML = "";
+        this.hideOrShowElement(this.formButtons);
     }
 }
--- src/components/brewery.js
+++ src/components/brewery.js
@@ -1,59 +1,41 @@
 class Brewery {
-    constructor(id, name, city, state, favorites) {
-        this.id = id;
+    constructor(name, city, state, beers) {
         this.name = name;
         this.city = city;
         this.state = state;
-        this.favorites = favorites;
-        this.adapter = new BreweriesAdapter();
+        this.favorites = beers;
     }
 
     createBreweryCard() {
-        const card = document.createElement('div')
-        card.className = "card"
-        const cardInfo = document.createElement('div')
-        cardInfo.className = "card-info"
-        const name = document.createElement('h1')
-        name.innerHTML = this.name
-        cardInfo.appendChild(name)
-        const cityState = document.createElement('p')
-        cityState.innerHTML = this.city + ', ' + this.state
-        cardInfo.appendChild(cityState)
-        const favHeader = document.createElement('h3')
-        favHeader.innerHTML = "Favorite Beers:"
-        cardInfo.appendChild(favHeader)
-        const ul = document.createElement('ul')
-        for (let favorite of this.favorites) {
-            let li = document.createElement('li')
-            li.innerHTML = favorite
-            ul.appendChild(li)
+        if (this.favorites) {
+            const card = document.createElement('div');
+            card.className = "card";
+            
+            const cardInfo = document.createElement('div');
+            cardInfo.className = "card-info";
+
+            const name = document.createElement('h1');
+            name.innerHTML = this.name;
+            cardInfo.appendChild(name);
+
+            const cityState = document.createElement('p');
+            cityState.innerHTML = this.city + ', ' + this.state;
+            cardInfo.appendChild(cityState);
+
+            const favHeader = document.createElement('h3');
+            favHeader.innerHTML = "Favorite Beers";
+            cardInfo.appendChild(favHeader);
+
+            const ul = document.createElement('ul');
+            for (let favorite of this.favorites) {
+                let li = document.createElement('li');
+                li.innerHTML = favorite;
+                ul.appendChild(li);
+            }
+            cardInfo.appendChild(ul);
+
+            card.appendChild(cardInfo);
+            document.querySelector("#brewery-card-container").appendChild(card);
         }
-        cardInfo.appendChild(ul)
-        const footer = document.createElement('div')
-        footer.className = 'card-footer'
-        const deleteBtn = document.createElement('button')
-        deleteBtn.id = 'remove-brewery'
-        deleteBtn.innerHTML = 'Remove Brewery'
-        console.log(this.adapter)
-        // deleteBtn.addEventListener("click", function() {
-        //     this.adapter.getBreweries();
-        //     this.adapter.removeBrewery();
-        //     console.log("Hello")
-        // })
-        const editBtn = document.createElement('button')
-        editBtn.id = 'edit-brewery'
-        editBtn.innerHTML = 'Edit Brewery'
-        footer.appendChild(deleteBtn)
-        footer.appendChild(editBtn)
-        card.appendChild(cardInfo)
-        card.appendChild(footer)
-        document.getElementById('brewery-card-container').appendChild(card)
     }
-
-    removeBreweryCard(){
-        const card= document.getElementsByClassName("card")
-        card.remove()
-    }
-
-
 }
